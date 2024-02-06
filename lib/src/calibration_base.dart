04e9d8940ed7b0cdf957509110f8135096278f8f
Kate Kuehl <katekuehl@gmail.com>
2024-02-06 13:35:46 -0600

    Add apply calibration curves

--- lib/src/calibration_base.dart
+++ lib/src/calibration_base.dart
@@ -136,6 +136,45 @@ List<double> linearCurve(List<CalibrationPoint> points) {
   return [b, m];
 }
 
+double _applyLinearValue(
+    double value, List<proto.CalibrationPoint> calibration) {
+  if (calibration.length < 2) {
+    // Handle error: not enough calibration data
+    return 0.0;
+  }
+
+  double a = calibration[0].uncalibrated[0];
+  double b = calibration[1].uncalibrated[0];
+
+  return a + b * value; // Apply linear calibration
+}
+
+double _applyExponentialValue(
+    double value, List<proto.CalibrationPoint> calibration) {
+  if (calibration.length < 3) {
+    // Handle error: not enough calibration data
+    return 0.0;
+  }
+
+  double a = calibration[0].uncalibrated[0];
+  double b = calibration[1].uncalibrated[0];
+  double c = calibration[2].uncalibrated[0];
+
+  return a * exp(b * value) + c; // Apply exponential calibration
+}
+
+double calibrateValue(proto.CurveType curveType, double value,
+    List<proto.CalibrationPoint> calibration) {
+  switch (curveType) {
+    case proto.CurveType.CURVE_LINEAR:
+      return _applyLinearValue(value, calibration);
+    case proto.CurveType.CURVE_EXPONENTIAL:
+      return _applyExponentialValue(value, calibration);
+    default:
+      return 0.0;
+  }
+}
+
 class CalibrationTemplate {
   final CurveType curveType;
   final List<Standard> standards;
